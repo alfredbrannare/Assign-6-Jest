1868b1c6a81ce57ddc539e25885bfc2f16520bcf
Alfred <alfred-brannarer@hotmail.com>
2024-10-02 17:46:36 +0200

    first commit

---
+++ __tests__/test.spec.js
@@ -0,0 +1,25 @@
+describe("toUpperCase test", () => {
+    function toUpperCase(test) {
+        const upperText = test.toUpperCase();
+        console.log(upperText);
+        return upperText;
+    }
+
+    //Describes what the test is suppoes to do
+    it("Hopefully this logs toUpperCase", () => {
+        // Track the console.log instead of output
+        console.log = jest.fn();
+
+        const input = "hElLo WoRlD";
+        const output = "HELLO WORLD";
+
+        // Call the function
+        const result = toUpperCase(input);
+
+        // Assertions
+        expect(result).toBe(output);
+
+        //Checks if console.log is called with the correct output
+        expect(console.log).toHaveBeenCalledWith(output);
+    });
+});
